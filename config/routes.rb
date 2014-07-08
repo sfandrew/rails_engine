3f8b7cf6bd147445d06b9743a3927d430fd29267
Andrew G <andrew@sfrent.net>
2014-07-08 13:51:07 -0700

    Testing a rails engine

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Blorgh::Engine.routes.draw do
+  	root to: "posts#index"
+
+	resources :posts do
+	resources :comments
+	end
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '2aba551947a8d5549ec94342d7592f5b153551c61b8f284b5e0e410644b85e4420c871f366d56a6927e892bff3013bc1ce6699170a49b2556126862d726dbef9'
